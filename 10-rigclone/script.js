166b8fd69713f3fb2f2e46b1cff894871189825c
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2026-05-22 23:29:05 +0530

    JS Practice

---
+++ 10-rigclone/script.js
@@ -0,0 +1,40 @@
+// JS Basic Practice
+
+// Quest 1
+// let age = prompt("Enter your age")
+
+// if (age === null) {
+//     console.error("You cancelled it")
+// }
+
+// else {
+//     if(age.trim()==="") console.error("Please enter your age"); 
+        
+//     else if(isNaN(age.trim())) console.error("Age should be a number")
+        
+//     else if (Number(age.trim())<0) console.error("Age should be positive")
+             
+//     else console.log(Number(age.trim())>=18? "Eligible" : "Not eligible") 
+// }
+
+// Ques -3
+// let password = "Harsh" 
+
+// let count = 0
+
+// let pass = prompt("Enter Password");
+// count++
+
+// while(pass !== password) {
+//     if(count === 3) {
+//         console.error("Password limit reached");
+//         break;
+//     }
+//         pass = prompt("Enter Password");
+//         count++;
+
+//     if(pass===password) {
+//         console.log("Access granted");
+//     }
+// }
+
