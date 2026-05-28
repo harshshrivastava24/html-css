ffd95494d813bfaf5f7c8f17491dfec89ac7217a
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2026-05-28 23:39:01 +0530

    section

--- 11-creator-practice/script.js
+++ 11-creator-practice/script.js
@@ -1,5 +1,6 @@
 let follower = document.querySelector(".follower")
-let list = document.querySelectorAll('a')
+let a = document.querySelectorAll('a')
+let list = document.querySelector(".list")
 
 window.addEventListener("mousemove", function(dets) {
     follower.style.top = dets.clientY + "px";
@@ -10,18 +11,21 @@ let menu = document.querySelector('.menu-i')
 let closing = document.querySelector('.close')
 
 menu.addEventListener("click", function() {
-    list.forEach((node)=> {
+    a.forEach((node)=> {
         node.style.display = "initial"
     })
 
     menu.style.display = "none"
     closing.style.display = "initial"
+    list.style.padding = 1 + "rem"
 })
 closing.addEventListener("click", function() {
-    list.forEach((node)=> {
+    a.forEach((node)=> {
         node.style.display = "none"
     })
 
     menu.style.display = "initial"
     closing.style.display = "none"
+    list.style.padding = 0 + "rem"
+
 })
